df1cb7a99513180f852987312e3eb02b58fe75b9
Cleber <cleber_batista2010@hotmail.com>
2022-01-06 23:47:28 -0300

    Criando aplicação Webflux com spring, e populando a base

---
+++ src/main/java/com/apirest/webflux/repository/PlaylistRepository.java
@@ -0,0 +1,8 @@
+package com.apirest.webflux.repository;
+
+import com.apirest.webflux.document.Playlist;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> {
+
+}
