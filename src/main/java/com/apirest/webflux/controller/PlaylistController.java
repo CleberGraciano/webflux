fe0ffe8a53ea76d6992a8d229504ad76ab100c32
Cleber <cleber_batista2010@hotmail.com>
2022-01-07 00:09:31 -0300

    Criando Controller

---
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -0,0 +1,37 @@
+package com.apirest.webflux.controller;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/playlists")
+public class PlaylistController {
+
+    private final PlaylistService playlistService;
+
+    @Autowired
+    public PlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    @GetMapping
+    public Flux<Playlist> getPlaylist(){
+        return playlistService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Mono<Playlist> getPlaylistId(@PathVariable String id){
+        return playlistService.findById(id);
+    }
+
+    @PostMapping
+    public Mono<Playlist> savePlalist(@RequestBody Playlist playlist){
+        return playlistService.save(playlist);
+    }
+
+
+}
