573e1d38f9e2b3d0fe192780e1bc858391b05b12
Cleber <cleber_batista2010@hotmail.com>
2022-01-07 00:33:46 -0300

    criando um serviço reativo funcional playlistHandler

---
+++ src/main/java/com/apirest/webflux/PlaylistHandler.java
@@ -0,0 +1,44 @@
+package com.apirest.webflux;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Component
+public class PlaylistHandler {
+
+    private final PlaylistService playlistService;
+
+    @Autowired
+    public PlaylistHandler(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    public Mono<ServerResponse> findAll(ServerRequest request){
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(playlistService.findAll(), Playlist.class);
+    }
+
+    public Mono<ServerResponse> findById(ServerRequest request){
+        String id = request.pathVariable("id");
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(playlistService.findById(id), Playlist.class);
+    }
+
+    public  Mono<ServerResponse> save(ServerRequest request){
+        final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromPublisher(playlist.flatMap(playlistService::save), Playlist.class));
+    }
+}
