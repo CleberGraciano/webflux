be5adb331ec64009591a5f1da72adab8ee862146
Cleber <cleber_batista2010@hotmail.com>
2022-01-07 00:40:17 -0300

    Criando router

---
+++ src/main/java/com/apirest/webflux/PlaylistRouter.java
@@ -0,0 +1,23 @@
+package com.apirest.webflux;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Configuration
+public class PlaylistRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(PlaylistHandler playlistHandler){
+        return RouterFunctions
+                .route(GET("/playlists").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::findAll)
+                .andRoute(GET("/playlists/{id}").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::findById)
+                .andRoute(POST("/playlists").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::save);
+    }
+}
